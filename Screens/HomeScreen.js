839eeec4954bb979d8e2b3465ca38c2bd1ff7e51
Lillian239 <lillian_lange@yahoo.com>
2025-10-30 12:00:55 +1100

    :art: replaced the react native core for text, button and added divider on HomeScreen.js

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,16 +1,24 @@
 // NOTE IMPORTED Libs FROM react
 import * as React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import {Text, Button, Divider} from 'react-native-paper'
+
 
 // NOTE MAIN body Component for the Home Screen
 export default function HomeScreen({ navigation}) {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Bing Bing Yahoo</Text>
-            <Button 
-                title="Go to Details"
-                onPress={ () => navigation.navigate('Details', {user: 'Guest'})}
-            />
+            <Text variant='headlineMedium' style={styles.homeMarg}>Home</Text>
+            <button mode='contained' color='Red' onPress={() => {}}>
+                Go to Details
+            </button>
+
+            <Divider style={StyleSheet.divider}/>
+
+            <button mode='outlined' icon='camera' onPress={() => {}}>
+                go to Gallert
+            </button>
+
         </View>
     );
 }
@@ -19,5 +27,12 @@ export default function HomeScreen({ navigation}) {
 // NOTE MAIN Styles ref
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 20, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 24, fontWeight: '600', marginBottom: 12 },
+  divider: {marginVertical: 16},
+  homeMarg: {marginBottom: 16},
 });
+
+/*CODE GRAVE
+    *Old title CSS for text on react native
+    *title: { fontSize: 24, fontWeight: '600', marginBottom: 12 },
+
+*/
